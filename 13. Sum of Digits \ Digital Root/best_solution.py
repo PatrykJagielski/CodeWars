c6d9fd45ab1ae440443e70777410c1966a88a70d
Patryk Jagielski <patryk1993xd@gmail.com>
2021-01-17 16:46:06 +0100

    solve 13. task

---
+++ 13. Sum of Digits \ Digital Root/best_solution.py
@@ -0,0 +1,12 @@
+def digital_root(n):
+    return n if n < 10 else digital_root(sum(map(int, str(n))))
+
+
+def digital_root(n):
+    return n % 9 or n and 9
+
+
+def digital_root(n):
+    while n > 9:
+        n = sum(map(int, str(n)))
+    return n
